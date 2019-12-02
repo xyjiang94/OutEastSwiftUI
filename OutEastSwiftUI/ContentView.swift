510050732dcbd711ef94695b6c60e0068c7c290c
xyjiang94 <xyjiang94@gmail.com>
2019-12-02 16:34:24 -0500

    Initial Commit

---
+++ OutEastSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OutEastSwiftUI
+//
+//  Created by Xinyi Jiang on 12/2/19.
+//  Copyright © 2019 Xinyi Jiang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OutEastSwiftUITests/OutEastSwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  OutEastSwiftUITests.swift
+//  OutEastSwiftUITests
+//
+//  Created by Xinyi Jiang on 12/2/19.
+//  Copyright © 2019 Xinyi Jiang. All rights reserved.
+//
+
+import XCTest
+@testable import OutEastSwiftUI
+
+class OutEastSwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
